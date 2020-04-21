b9c195a3b09a565c422569a74cb7f81da55767ee
adawyj97 <49011074+adawyj97@users.noreply.github.com>
2020-04-20 22:48:24 -0400

    transportation sliders

--- Project/js/main.js
+++ Project/js/main.js
@@ -51,8 +51,73 @@ var islandLoc = {
 };
 
 $(document).ready(function() {
-  $.ajax('https://raw.githubusercontent.com/adawyj97/Capstone-Project/master/Data/Tuvalu_grid.json').done(function(dataset) {
+  $("#slidervalue").text(`${$(".slider").val()} m`);
+  $.ajax('https://raw.githubusercontent.com/adawyj97/Capstone-Project/master/Data/DEM_grid_attri.json').done(function(dataset) {
       data = JSON.parse(dataset);
+      map.on('load', function() {
+        map.addSource('elevation', {
+          type: 'geojson',
+          data: data
+        });
+
+        // Insert the layer beneath any symbol layer.
+        var layers = map.getStyle().layers;
+        var labelLayerId;
+        for (var i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol') {
+            labelLayerId = layers[i].id;
+            break;
+          }
+        }
+
+        map.addLayer(
+        {
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+              ],
+              'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+              ],
+              'fill-extrusion-opacity': 0.6
+            }
+          }, labelLayerId
+          );
+
+
+          map.addLayer({'id': 'fishnet',
+            'type': 'fill',
+            'source': 'elevation',
+            'layout': {},
+            'paint': {
+              'fill-color': '#088',
+              'fill-opacity': 0.8
+            },
+            'filter': ["==", `bel_${$("#myRange").val()}0_con`.slice(0, 10), 1]
+          }, '3d-buildings');
+        });
+
       document.getElementById("islands").onchange = function() {
         var selected = $("#islands option:selected").text();
         map.flyTo({
@@ -61,68 +126,35 @@ $(document).ready(function() {
           essential: true
         });
       };
+
+      document.getElementById("myRange").onchange= function() {
+        var elev = $("#myRange").val();
+        var predicate = `bel_${elev}0_con`.slice(0, 10);
+        $("#slidervalue").text(`${elev} m`);
+        map.setFilter('fishnet', ["==", predicate, 1]);
+      };
+
+      $("#transportation_switch").on('change', function() {
+        if ($(this).is(':checked')) {
+          map.setPaintProperty('fishnet', 'fill-color',  [
+            'match',
+            ['get', 'Tran_recla'],
+            0,
+            '#f0f9e8',
+            1,
+            '#ccebc5',
+            2,
+            '#a8ddb5',
+            3,
+            '#7bccc4',
+            4,
+            '#43a2ca',
+            '#0868ac'
+          ]);
+        }
+        else {
+          map.setPaintProperty('fishnet', 'fill-color', '#088');
+        }
+      });
   });
 });
-
-map.on('load', function() {
-  map.addSource('elevation', {
-    type: 'geojson',
-    data: data
-  });
-
-  // Insert the layer beneath any symbol layer.
-  var layers = map.getStyle().layers;
-  var labelLayerId;
-  for (var i = 0; i < layers.length; i++) {
-    if (layers[i].type === 'symbol') {
-      labelLayerId = layers[i].id;
-      break;
-    }
-  }
-
-  map.addLayer(
-  {
-    'id': '3d-buildings',
-    'source': 'composite',
-    'source-layer': 'building',
-    'filter': ['==', 'extrude', 'true'],
-    'type': 'fill-extrusion',
-    'minzoom': 15,
-    'paint': {
-      'fill-extrusion-color': '#aaa',
-      // use an 'interpolate' expression to add a smooth transition effect to the
-      // buildings as the user zooms in
-      'fill-extrusion-height': [
-        'interpolate',
-        ['linear'],
-        ['zoom'],
-        15,
-        0,
-        15.05,
-        ['get', 'height']
-        ],
-        'fill-extrusion-base': [
-        'interpolate',
-        ['linear'],
-        ['zoom'],
-        15,
-        0,
-        15.05,
-        ['get', 'min_height']
-        ],
-        'fill-extrusion-opacity': 0.6
-      }
-    }, labelLayerId
-    );
-
-
-    map.addLayer({'id': 'fishnet',
-      'type': 'fill',
-      'source': 'elevation',
-      'layout': {},
-      'paint': {
-        'fill-color': '#088',
-        'fill-opacity': 0.5
-      }
-    }, '3d-buildings');
-  });
